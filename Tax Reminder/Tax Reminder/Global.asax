63d503f7fac5f635f740a71ebe32ce2820e3f587
nafeeur10 <nafeeur.kuet.cse6@gmail.com>
2016-12-19 22:34:12 +0600

    First Commit And Push

---
+++ Tax Reminder/Tax Reminder/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tax_Reminder.MvcApplication" Language="C#" %>
---
+++ Tax Reminder/Tax Reminder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tax_Reminder.Startup))]
+namespace Tax_Reminder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
